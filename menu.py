3e21d05e43732b9f3fd3e224a45e06f755c66d0b
Julio <julio-lafre@hotmail.com>
2023-07-20 23:03:11 -0300

    Incrementando MENU

--- menu.py
+++ menu.py
@@ -1,2 +1,40 @@
-while True:
-    pass
+from main import *
+opcao = None
+
+while opcao != 0:
+    print("""
+        1 - Cadastrar Veiculos
+        2 - Listar Veículos
+        3 - Editar Veículos
+        4 - Remover Veículos
+        0 - Sair
+    """)
+
+    try:
+        opcao = int(input("Digite a opção desejada: "))
+    except ValueError:
+        print("Insira um número válido.")
+        input("Pressione ENTER para continuar... ")
+        
+        
+    
+    if opcao == 1:
+        CadastrarCarro()
+        print("Carro cadastrado com sucesso!")
+
+    elif opcao == 2:
+        pass
+    
+    elif opcao == 3:
+        pass
+
+    elif opcao == 4:
+        pass
+
+    elif opcao == 0:
+        print("Encerrando...")
+        break
+
+    else:
+        print("Insira uma opcao válida.")
+        input("Pressione ENTER para continuar... ")
