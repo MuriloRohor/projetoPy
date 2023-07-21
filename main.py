70b36e1b444cbab5090029c9f7a13830b2da54c3
MuriloRohor <murilo.marquezini@gmail.com>
2023-07-21 11:13:37 -0300

    Função Listar e Função Pausar Console

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ carros = lerArquivo()
 
 
 def CadastrarCarro():
+    id = len(carros) + 1
     marca = input("Digite a marca do veículo: ")
     modelo = input("Digite o modelo do veículo: ")
     cor = input("Digite a cor do veículo: ")
     ano = input("Digite o ano do veículo: ")
     carro_molde = {
+    "id" : id,
     "marca" : marca,
     "modelo" : modelo,
-    "Cor" : cor,
-    "Ano" : ano,
+    "cor" : cor,
+    "ano" : ano,
     }
     carros.append(carro_molde)
     salvarArquivo(carros)
 
+def ListarCarros():
+    for carro in carros:
+        print(f"""
+            Id     : {carro['id']}
+            Marca  : {carro['marca']}
+            Modelo : {carro['modelo']}
+            Cor    : {carro['cor']}
+            Ano    : {carro['ano']}
+            """)
 
-
+def Pausar():
+    input("Pressione ENTER para continuar...")
 
 
 
--- menu.py
+++ menu.py
@@ -1,2 +1,42 @@
-while True:
-    pass
+from main import *
+opcao = None
+
+while opcao != 0:
+    print("""
+        1 - Cadastrar Veiculos
+        2 - Listar Veículos
+        3 - Editar Veículos
+        4 - Remover Veículos
+        0 - Sair
+    """)
+
+    try:
+        opcao = int(input("Digite a opção desejada: "))
+    except ValueError:
+        print("Insira um número válido.")
+        Pausar()
+        
+        
+    
+    if opcao == 1:
+        CadastrarCarro()
+        print("Carro cadastrado com sucesso!")
+        Pausar()
+
+    elif opcao == 2:
+        ListarCarros()
+        Pausar()
+    
+    elif opcao == 3:
+        pass
+
+    elif opcao == 4:
+        pass
+
+    elif opcao == 0:
+        print("Encerrando...")
+        break
+
+    else:
+        print("Insira uma opcao válida.")
+        Pausar()
